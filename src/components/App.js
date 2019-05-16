fe4826453634b1550d00d845a492b080f29a7e69
victorgh <victor.gonzalez@soprasteria.com>
2019-05-16 11:52:32 +0200

    feat: video detail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
 
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -17,12 +18,17 @@ class App extends React.Component {
         this.setState({videos: response.data.items});
     };
 
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
+    }
+
     render () {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoDetail video={this.state.selectedVideo} />
                 I have {this.state.videos.length} videos
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     };
