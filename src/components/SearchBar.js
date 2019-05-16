7b40872e24405f8d2699037ecc5e98e545c4e516
victorgh <victor.gonzalez@soprasteria.com>
2019-05-16 10:04:44 +0200

    first components

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+class SearchBar extends React.Component {
+    render () {
+        return (
+            <div>SearchBar</div>
+        );
+    }
+}
+
+
+export default App;
